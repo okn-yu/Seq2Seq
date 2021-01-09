9f36c5dfa9b6ce4373634133e10da218797a1a67
okn-yu <okn@gmail.com>
2021-01-09 15:30:53 +0000

    updated.

--- src/endeco.py
+++ src/endeco.py
@@ -1,13 +1,13 @@
 import torch.nn as nn
 
-from src.data_gen import toID
+from src.data_gen import to_ID
 
 
 class Encoder(nn.Module):
-    def _init__(self, vocab_size, embedding_dim, hidden_dim):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
         super(Encoder, self).__init__()
         self.hidden_dim = hidden_dim
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=toID()[" "])
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=to_ID()[" "])
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
 
     def forward(self, sequence):
@@ -23,8 +23,8 @@ class Decoder(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim):
         super(Decoder, self).__init__()
         self.hidden_dim = hidden_dim
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=toID()[" "])
-        self.word_embeddings = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=to_ID()[" "])
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
         self.hidden2linear = nn.Linear(hidden_dim, vocab_size)
 
     def forward(self, sequence, encoder_state):
